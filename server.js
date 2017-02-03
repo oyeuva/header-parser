11a5c455e89e15e670b81a545ad6e1610922c7bf
uday wakte <uday.wakte@gmail.com>
2017-02-03 08:49:08 +0000

    updating server.js functionality

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 var express = require('express');
+var os = require('os');
+var app = express();
+
+app.get('/', function(req, res){
+    var ipaddress = os.networkInterfaces().eth0[0].address;
+    var language = req.acceptsLanguages('en-US','en-GB', 'ru', 'uk', 'ru-mo', 'be' ); 
+    //var language = req.headers['accept-language'];//req.acceptsLanguages;
+    var software = os.platform()+': '+ os.release();
+    
+    var result = {
+        ipaddress: ipaddress,
+        language: language,
+        software: software
+    };
+    //var ans = JSON.parse(result);
+    res.json(result);
+});
+
+app.listen(process.env.PORT|| 8080, function(){
+    console.log('port is 8080!');
+});
